80b6aa4939216dbdddd2b07b2f71f6827f09f48d
8jackson6264 <gnr.axlrose.6.2.1962@gmail.com>
2020-05-05 16:42:16 +0300

    logic done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Calendar;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double lat = 43.205847;
+        double lon = 23.553495;
+
+        BarList barList = new BarList();
+
+        barList.printClosestBars(lat, lon);
+
+        barList.printMap(lat, lon);
+
+        System.out.println(returnTime());
+
+        System.out.println(barList.bars[2].getFormattedWorkingTime());
+    }
+
+    public static double returnTime(){
+        Calendar calendar = Calendar.getInstance();
+
+        return calendar.get(Calendar.HOUR_OF_DAY) + ((double)calendar.get(Calendar.MINUTE))/60;
     }
 }
